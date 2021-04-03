2628c215341566dadde8a61aa2bc36593bb86bc0
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-04-03 15:35:40 -0400

    Fixing webscraper
    yep

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+URL = 'https://www.indeed.com/jobs?q=software+developer&l=Concord%2C+MA'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
-job_elems = results.find_all('section', class_='card-content')
+results = soup.find(id='resultsBodyContent')
+job_elems = results.find_all(
+    'div', class_='jobsearch-SerpJobCard unifiedRow row result clickcard')
 for job_elem in job_elems:
-    # Each job_elem is a new BeautifulSoup object.
-    # You can use the same methods on it as you did before.
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
+    title_elem = job_elem.find('a', class_='jobtitle turnstileLink ')
+    company_elem = job_elem.find('span', class_='company')
+    location_elem = job_elem.find(
+        'div', class_='location accessible-contrast-color-location')
     if None in (title_elem, company_elem, location_elem):
         continue
     print(title_elem.text.strip())
