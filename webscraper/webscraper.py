f14c21c061a00db89ed3429eaff08e118c5eea9f
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-04-08 14:15:04 -0400

    Redoing webscraper
    Redoing the webscraper so that it not so bad

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -1,21 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 URL = 'https://www.indeed.com/jobs?q=software+developer&l=Concord%2C+MA'
-page = requests.get(URL)
+page = urlopen(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='resultsBodyContent')
-job_elems = results.find_all(
-    'div', class_='jobsearch-SerpJobCard unifiedRow row result clickcard')
-for job_elem in job_elems:
-    title_elem = job_elem.find('a', class_='jobtitle turnstileLink ')
-    company_elem = job_elem.find('span', class_='company')
-    location_elem = job_elem.find(
-        'div', class_='location accessible-contrast-color-location')
-    if None in (title_elem, company_elem, location_elem):
-        continue
-    print(title_elem.text.strip())
-    print(company_elem.text.strip())
-    print(location_elem.text.strip())
-    print()
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+title_index = html.find("se","<title>")
+print(html)
