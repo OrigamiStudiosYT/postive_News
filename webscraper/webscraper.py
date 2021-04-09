e9792eedb6828b70be4d88f84a11488c1f034064
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-04-09 11:32:30 -0400

    Relearned how to write files
    Relearned how to read and write files in python. It has allowed things to be real fancy and allowed for the two programs to start to read from each other. Thats pretty good.

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -2,11 +2,13 @@ from urllib.request import urlopen
 
 URL = 'https://origamistudiosyt.github.io/'
 page = urlopen(URL)
-
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-title_index = html.find("se", "<title>")
-file = open("webscrap.txt", "w")
-file.write(html)
-file.close()
-print(html)
+try:
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    file = open("webscraper/webscrap.txt", "w")
+    file.write(html)
+except:
+    print("Something has failed and this is not epic")
+finally:
+    file.close()
+    print(html)
