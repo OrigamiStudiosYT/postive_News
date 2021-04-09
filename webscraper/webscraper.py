e8bc05dc6d78b219643b5373c1e586f3c7b6c70b
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-04-09 14:18:01 -0400

    Updates

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
+from requests_html import HTMLSession
+session = HTMLSession()
 
-URL = 'https://origamistudiosyt.github.io/'
-page = urlopen(URL)
+URL = session.get('https://www.nytimes.com/section/technology')
+page = URL.html.render()
 try:
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
