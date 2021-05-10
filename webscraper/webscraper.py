6cbf5264caed8d8791557717c806505dadac97d5
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-05-10 12:49:55 -0400

    edits

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -14,7 +14,7 @@ def opening():
 def searching(file):
     try:
         html = page.html.find(
-            ".css-xta2bx", first=True)
+            ".css-xta2bx")[range(1,3)]
         html = html.text
         html = html.replace("Photo", " ")
         html = html.replace("Credit", "Photo credit: ")
