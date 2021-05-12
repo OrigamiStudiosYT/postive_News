d1f51f648672bfffb3af4d7937c0a7b7d6074fc0
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-05-12 10:03:48 -0400

    Making injector
    Starting building the injector, it should be able to give one paragraph at a time and should be able to feed into the html document that is attached

--- injector.py
+++ injector.py
@@ -1,8 +1,35 @@
-file = open("webscraper/webscrap.txt", "r")
+def opening():
+    try:
+        file = open("webscraper/webscrap.txt", "r")
+    except:
+        print("Opening the file has failed")
+    return file
 
-try:
-    print(file.read())
-except:
-    print("Gathering has failed")
-finally:
-    file.close()
+
+def reading(file):
+    lines = file.readlines()
+    for line in lines:
+        if line == " \n":
+            print("Empty Line")
+        else:
+            html = line.strip()
+    return html
+
+
+def injecting(html):
+    for x in range(4):
+        html = reading(file)
+        print("Something")
+
+
+def closing(file):
+    try:
+        file.close()
+    except:
+        print("Closing or exiting the file has failed")
+
+
+file = opening()
+readed = injecting(html)
+print(html)
+closing(file)
