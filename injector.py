f8b34d08580f6328e35771ceff207a3d2221722f
OrigamiStudiosYT <49206355+OrigamiStudiosYT@users.noreply.github.com>
2021-05-12 14:13:06 -0400

    Still not finished but half is working
    The webscraper is working, which was the hardest part, need to get the injector working, but that should be the fine part. Lets hope that things work out in the end.

--- injector.py
+++ injector.py
@@ -1,3 +1,8 @@
+from bs4 import BeautifulSoup
+
+
+
+
 def opening():
     try:
         file = open("webscraper/webscrap.txt", "r")
@@ -13,14 +18,31 @@ def injecting():
 
 
 def reading(file):
-    lines = file.readlines()
-    htmla = []
-    for line in lines:
-        if line == " \n":
-            print("Empty Line")
-        else:
-            htmla.append(line.strip())
-    return htmla
+    with open("index.html", encoding="utf-8", mode="r+") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+    #lines = file.readlines()
+    #htmla = []
+    pclassses1 = soup.find("p", class_="Text1")
+    pclassses2 = soup.find("p", class_="Text2")
+    pclassses3 = soup.find("p", class_="Text3")
+    pclassses4 = soup.find("p", class_="Text4")
+
+    pclassses1 = pclassses1.replaceWith("<p class='Text1'> line2 </p>")
+    pclassses2 = pclassses2.replaceWith("<p class='Text2'> line2 </p>")
+    pclassses3 = pclassses3.replaceWith("<p class='Text3'> line3 </p>")
+    pclassses4 = pclassses4.replaceWith("<p class='Text4'> line4 </p>")
+
+    fp.write(str(pclassses1))
+    fp.write(str(pclassses2))
+    fp.write(str(pclassses3))
+    fp.write(str(pclassses4))
+
+    # for line in lines:
+    #    if line == " \n":
+    #        print("Empty Line")
+    #    else:
+    #        htmla.append(line.strip())
+    # return htmla
 
 
 def closing(file):
@@ -31,5 +53,6 @@ def closing(file):
 
 
 file = opening()
-readed = injecting()
+reading(file)
+#readed = injecting()
 closing(file)
